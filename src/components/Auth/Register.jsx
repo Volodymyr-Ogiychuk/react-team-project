d9bc656fbc1bf0b4fe3b81b065d4b839715fd301
Євгеній <108029808+Zhe1a@users.noreply.github.com>
2022-11-29 16:56:06 +0200

    +++

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,20 +1,77 @@
-import { Formik, Field, Form } from "formik";
-import { NavLink } from 'react-router-dom';
+import { Formik, Field, Form } from 'formik';
+import { NavLink, useNavigate } from 'react-router-dom';
+
+import { useDispatch } from 'react-redux';
+import { RegisterApi } from 'redux/AuthRedux/operations';
+
+import s from './Login.module.css';
+
 const Register = () => {
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   return (
-    <div>
-    <h1>Wallet</h1>
+    <div className={s.box}>
+      <h1>Wallet</h1>
       <Formik
-        initialValues={{ password: '', email: '' }}
+        initialValues={{ password: '', email: '', username: '' }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        onSubmit={e => {
+          const password = e.password;
+          if (password.length > 6 && password.length < 12){
+             dispatch(RegisterApi(e))
+          } else{
+            console.log("false")
+          }
+            navigate('/login');
+        }}
       >
-        <Form>
-        <Field name="email" type="email" required/>
-          <Field name="password" type="password" required/>
-          <Field name="password" type="password" required/>
-          <Field name="name" type="name" required/>
-          <button type="submit">Register</button>
-          <NavLink to="/login">Log in</NavLink>
+        <Form className={s.form}>
+          <Field
+            name="email"
+            type="email"
+            required
+            className={s.field}
+            placeholder="E-mail"
+          />
+          <Field
+            name="password"
+            type="password"
+            required
+            className={s.field}
+            placeholder="Password"
+          />
+          <Field
+            name="password"
+            type="password"
+            required
+            className={s.field}
+            placeholder="Confirm password"
+          />
+          <Field
+            name="username"
+            type="username"
+            required
+            className={s.field}
+            placeholder="First name"
+          />
+          <button type="submit" className={s.button}>
+            Register
+          </button>
+          <NavLink to="/login" className={s.link}>
+            Log in
+          </NavLink>
         </Form>
       </Formik>
     </div>
--- src/redux/AuthRedux/AuthSlice.jsx
+++ src/redux/AuthRedux/AuthSlice.jsx
@@ -1,14 +1,15 @@
 const { createSlice } = require('@reduxjs/toolkit/dist');
-const { LoginApi, fetchCurrentUser } = require('./operations');
+const { LoginApi, fetchCurrentUser, RegisterApi, ResetApi } = require('./operations');
 
 const handlePending = state => {
-
+  state.registerUser = false;
     state.isLoggedIn = false;
 };
 
 const handleRejected = (state, action) => {
   state.error = action.payload;
   state.isLoggedIn = false;
+  state.registerUser = false;
 };
 
 const auth = createSlice({
@@ -19,6 +20,7 @@ const auth = createSlice({
     token: null,
     error: null,
     balance: 0,
+    registerUser: false
   },
   extraReducers: {
     [LoginApi.pending]: handlePending,
@@ -29,6 +31,24 @@ const auth = createSlice({
       state.isLoggedIn = true;
       state.balance = action.payload.balance;
       state.error = null;
+    },
+    [RegisterApi.pending]: handlePending,
+    [RegisterApi.rejected]: handleRejected,
+    [RegisterApi.fulfilled](state, action) {
+      state.username = action.payload.username;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+      state.balance = action.payload.balance;
+      state.error = null;
+    },
+    [ResetApi.pending]: handlePending,
+    [ResetApi.rejected]: handleRejected,
+    [ResetApi.fulfilled](state, action) {
+        state.username = '';
+        state.token = null;
+        state.isLoggedIn = false;
+        state.balance = 0;
+        state.error = null;
     },
     [fetchCurrentUser.pending]: handlePending,
     [fetchCurrentUser.rejected]: handleRejected,
