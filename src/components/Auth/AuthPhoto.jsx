40be65204a177feef636863bde205e3de8680bb2
OlegKushnir <kushnir.oleg87@gmail.com>
2022-12-05 10:06:29 +0200

    fix

--- src/components/Auth/AuthPhoto.jsx
+++ src/components/Auth/AuthPhoto.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 const Photo = ({ img }) => {
   return (
     <div className={s.aside}>
+      {/* eslint-disable-next-line */}
       <img src={img} alt="Photo" className={s.image} />
       <h2 className={s.title}>Finance App</h2>
     </div>
